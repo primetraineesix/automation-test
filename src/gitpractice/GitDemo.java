60ffdeeff177a85bcba78e21d65d1440ab051756
primetraineesix <primetraineesix@gmail.com>
2021-10-30 16:43:21 +0100

    Automation Project

---
+++ src/gitpractice/GitDemo.java
@@ -0,0 +1,10 @@
+package gitpractice;
+
+/**
+ * Created by Jay Vaghani
+ */
+public class GitDemo {
+    public static void main(String[] args) {
+        System.out.println(" This is sample automation project");
+    }
+}
