63ee9a7547f5da42d7e61bb9f1531d2bc744eab0
primetraineesix <primetraineesix@gmail.com>
2021-10-30 17:03:13 +0100

    Git demo3

---
+++ src/gitpractice/GitDemo3.java
@@ -0,0 +1,11 @@
+package gitpractice;
+
+/**
+ * Created by Jay Vaghani
+ */
+public class GitDemo3 {
+
+    public static void main(String[] args) {
+        System.out.println("Demo three");
+    }
+}
