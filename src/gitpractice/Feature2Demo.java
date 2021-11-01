6e125e3de75447f54928564e214ce481964b30b6
primetraineesix <primetraineesix@gmail.com>
2021-11-01 20:49:01 +0000

    Feature 2 created

---
+++ src/gitpractice/Feature2Demo.java
@@ -0,0 +1,10 @@
+package gitpractice;
+
+/**
+ * Created by Jay
+ */
+public class Feature2Demo {
+    public static void main(String[] args) {
+        System.out.println("Feature 2 created");
+    }
+}
